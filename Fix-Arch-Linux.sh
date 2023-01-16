a802babf9e22ee2e95a5271cdc012e378aa07ea8
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-01-16 13:42:40 -0500

    Add files via upload

---
+++ Fix-Arch-Linux.sh
@@ -0,0 +1,72 @@
+read -r -p "Are you recieving the error 'unable to lock database' [Y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        rm /var/lib/pacman/db.lck
+        ;;
+    *)
+        
+        ;;
+esac
+read -r -p "Are you recieving the error 'failed to synchronize all databases' [Y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        rm -f /var/lib/pacman/sync/
+        ;;
+    *)
+        
+        ;;
+esac
+read -r -p "Are you recieving the error 'unable to lock database' for Pamac? [Y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        rm /var/tmp/pamac/dbs/db.lck
+        ;;
+    *)
+        
+        ;;
+esac
+read -r -p "Are you recieving the error 'failed to synchronize all databases' for Pamac? [Y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        rm -r /var/lib/pacman/sync/ 
+        ;;
+    *)
+        
+        ;;
+esac
+read -r -p "Are you having GPG key errors? [Y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        mv /etc/pacman.d/gnupg{,.bak} && pacman-key --populate archlinux && pacman-key --populate artix && pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key FBA220DFC880C036 && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && pacman-key --populate manjaro
+        ;;
+    *)
+        
+        ;;
+esac
+read -r -p "After an update can you not login to your desktop enviroment? [Y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        update-grub
+        ;;
+    *)
+        
+        ;;
+esac
+read -r -p "Are most packages corrupted on the system? [Y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        pacman -Syu --overwrite "*" $(pacman  -Qnq)
+        ;;
+    *)
+        
+        ;;
+esac
+read -r -p "Are s6 services not launching on the system? [Y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        s6-db-reload
+        ;;
+    *)
+        
+        ;;
+esac
