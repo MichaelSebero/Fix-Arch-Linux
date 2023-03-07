5e854b58195bf7e6cb99eb7e9dea56bd46f0277c
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-03-06 21:41:39 -0500

    Update Fix-Arch-Linux.sh

--- Fix-Arch-Linux.sh
+++ Fix-Arch-Linux.sh
@@ -46,7 +46,7 @@ read -r -p "
 Are you having GPG key errors? [Y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-        mv /etc/pacman.d/gnupg{,.bak} && pacman-key --init && pacman-key --populate archlinux && pacman-key --populate artix && pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key FBA220DFC880C036 && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+        mv /etc/pacman.d/gnupg{,.bak} && pacman-key --init && pacman-key --populate archlinux && pacman-key --populate artix && pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key FBA220DFC880C036 && pacman -U --noconfirm --needed 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
         ;;
     *)
         
