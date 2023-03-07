7a5ad24222ecbf3704820e5a9a719fcb2094f452
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-03-06 21:35:16 -0500

    Update Fix-Arch-Linux.sh

--- Fix-Arch-Linux.sh
+++ Fix-Arch-Linux.sh
@@ -1,6 +1,9 @@
-su -c'pacman -Syu --noconfirm --needed'
+pacman -Syu --noconfirm --needed
 
-read -r -p "Are you recieving the error 'unable to lock database' [Y/N] " response
+read -r -p "
+------------------------------------------------------------------------------------
+
+Are you recieving the error 'unable to lock database' [Y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
         rm /var/lib/pacman/db.lck
@@ -9,7 +12,8 @@ case "$response" in
         
         ;;
 esac
-read -r -p "Are you recieving the error 'failed to synchronize all databases' [Y/N] " response
+read -r -p "
+Are you recieving the error 'failed to synchronize all databases' [Y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
         rm -rf /var/lib/pacman/sync/ && pacman -Sy
@@ -18,7 +22,8 @@ case "$response" in
         
         ;;
 esac
-read -r -p "Are you recieving the error 'unable to lock database' for Pamac? [Y/N] " response
+read -r -p "
+Are you recieving the error 'unable to lock database' for Pamac? [Y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
         rm /var/tmp/pamac/dbs/db.lck
@@ -27,7 +32,8 @@ case "$response" in
         
         ;;
 esac
-read -r -p "Are you recieving the error 'failed to synchronize all databases' for Pamac? [Y/N] " response
+read -r -p "
+Are you recieving the error 'failed to synchronize all databases' for Pamac? [Y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
         rm -rf /var/lib/pacman/sync/ 
@@ -36,7 +42,8 @@ case "$response" in
         
         ;;
 esac
-read -r -p "Are you having GPG key errors? [Y/N] " response
+read -r -p "
+Are you having GPG key errors? [Y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
         mv /etc/pacman.d/gnupg{,.bak} && pacman-key --init && pacman-key --populate archlinux && pacman-key --populate artix && pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key FBA220DFC880C036 && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
@@ -45,7 +52,8 @@ case "$response" in
         
         ;;
 esac
-read -r -p "After an update can you not login to your desktop enviroment? [Y/N] " response
+read -r -p "
+After an update can you not login to your desktop enviroment? [Y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
         update-grub
@@ -54,7 +62,8 @@ case "$response" in
         
         ;;
 esac
-read -r -p "Are most packages corrupted on the system? [Y/N] " response
+read -r -p "
+Are most packages corrupted on the system? [Y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
         pacman -Syu --overwrite "*" $(pacman  -Qnq)
